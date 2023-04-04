6f827ecedbe5240bf7b282160abb1f9406e26e72
CumMblLLl <nobbiland@gmail.com>
2023-04-04 11:11:59 +0300

    fix errors

--- src/redux/createTest/types.ts
+++ src/redux/createTest/types.ts
@@ -35,12 +35,26 @@ export interface GradesValuesType {
   testGrades: TestGradeItem[];
 }
 
+export interface SettestGradesAction {
+  type: CreateActionTyped.SET_TEST_GRADES;
+  values: GradesValuesType;
+}
+
 export interface QuestionsValuesType {
   testQuestions: TestQuestionItem[];
+}
+
+export interface SettestQuestions {
+  type: CreateActionTyped.SET_QUESTIONS_TEST;
+  values: QuestionsValuesType;
 }
 
 export interface CancelCreateTest {
   type: CreateActionTyped.CLEAR_TEST_STATE;
 }
 
-export type ActionCreateTest = SetNameTestAction | CancelCreateTest;
+export type ActionCreateTest =
+  | SetNameTestAction
+  | SettestGradesAction
+  | SettestQuestions
+  | CancelCreateTest;
